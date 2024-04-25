be2dd3ea22eefcbca4211977f08bad6e57be9b7f
mahmoud atef <109464635+MahmoudAtefFarook@users.noreply.github.com>
2024-04-26 02:59:35 +0300

    Format tafaseer.

--- core_functions/tafaseer.py
+++ core_functions/tafaseer.py
@@ -42,7 +42,8 @@ class TafaseerManager:
         self._cursor.execute(query, [ayah_number])
         result = self._cursor.fetchone()
         if result:
-            return result["text"].replace(".", ".\n")
+            tetx = result[0].replace(".", ". \n").strip()
+            return tetx
         else:
             return ""
 
