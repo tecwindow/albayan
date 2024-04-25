45bc408f9411ee1521cd4124cf62f7f5eaf0fff9
mahmoud atef <109464635+MahmoudAtefFarook@users.noreply.github.com>
2024-04-26 01:01:21 +0300

    Create tafaseer dialog.

---
+++ ui/dialogs/tafaseer_Dialog.py
@@ -0,0 +1,54 @@
+import sys
+from PyQt6.QtWidgets import (
+    QWidget,
+    QHBoxLayout, 
+    QVBoxLayout, 
+    QPushButton, 
+    QDialog, 
+    QApplication,
+    QFileDialog,
+)
+from PyQt6.QtGui import QIcon
+from ui.widgets.qText_edit import ReadOnlyTextEdit
+
+
+class TafaseerDialog(QDialog):
+    def __init__(self, parent, title):
+        super().__init__(parent)
+        self.parent = parent
+        self.setWindowTitle(title)
+        self.setGeometry(100, 100, 300, 200)
+
+
+        self.layout = QVBoxLayout(self)
+        
+        self.text_edit = ReadOnlyTextEdit(self)
+        self.layout.addWidget(self.text_edit)
+
+        self.button_layout = QVBoxLayout()
+
+        self.copy_button = QPushButton("Copy")
+        self.copy_button.clicked.connect(self.copy_content)
+        self.button_layout.addWidget(self.copy_button)
+
+        self.save_button = QPushButton("Save")
+        self.save_button.clicked.connect(self.save_content)
+        self.button_layout.addWidget(self.save_button)
+
+        self.close_button = QPushButton("Close")
+        self.close_button.clicked.connect(self.close)
+        self.button_layout.addWidget(self.close_button)
+
+        self.layout.addLayout(self.button_layout)
+
+    def copy_content(self):
+        self.copied_content = self.text_edit.toPlainText()
+
+    def save_content(self):
+        options = QFileDialog.Options()
+        file_name, _ = QFileDialog.getSaveFileName(
+            self, "Save File", "", "Text Files (*.txt);;All Files (*)", options=options
+        )
+        if file_name:
+            with open(file_name, "w") as file:
+                file.write(self.text_edit.toPlainText())
