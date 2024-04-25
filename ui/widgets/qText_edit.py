e4ea10d8fa355141d82ab0c08a216e512f6651f6
mahmoud atef <109464635+MahmoudAtefFarook@users.noreply.github.com>
2024-04-26 01:00:31 +0300

    create ReadOnlyTextEdit.

---
+++ ui/widgets/qText_edit.py
@@ -0,0 +1,10 @@
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QTextEdit
+
+
+class ReadOnlyTextEdit(QTextEdit):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setReadOnly(True)
+        self.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByKeyboard| Qt.TextInteractionFlag.TextSelectableByMouse)
+
