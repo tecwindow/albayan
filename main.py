6d5360ee449e2572502fa9b7401afa967ae8553b
mahmoud atef <109464635+MahmoudAtefFarook@users.noreply.github.com>
2024-04-26 05:11:02 +0300

    Add logging and optmize the error handling.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 # Change the working dir to current dir
 current_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
 os.chdir(current_dir)
+from utils.logger import Logger
+sys.excepthook = Logger.my_excepthook
 from PyQt6.QtWidgets import QApplication, QMessageBox
 from PyQt6.QtCore import QTimer
 from ui.quran_interface import QuranInterface
@@ -16,7 +18,8 @@ def main():
         QTimer.singleShot(200, main_window.quran_view.setFocus)
         sys.exit(app.exec())
     except Exception as e:
-        QMessageBox.critical(None, "Error", str(e))
+        Logger.error(str(e))
+        QMessageBox.critical(None, "Error", "حدث خطأ. يرجى الاتصال بالدعم للحصول على المساعدة.")
 
 if __name__ == "__main__":
     main()
---
+++ utils/logger.py
@@ -0,0 +1,55 @@
+import logging
+import traceback
+import os
+import ctypes
+import sys
+
+
+class Logger:
+	last_logging_status = None
+
+	@classmethod
+	def initialize_logger(cls):
+		current_logging_status = "True"
+		if current_logging_status != cls.last_logging_status:
+			if current_logging_status == "True":
+				mode = "a"
+			else:
+				mode = "w"
+			logging.basicConfig(filename=os.path.join("albayan.log"),
+level=logging.INFO,
+					filemode=mode,
+					format="(%(asctime)s) | %(name)s | %(levelname)s => '%(message)s'")
+		cls.last_logging_status = current_logging_status
+
+	@classmethod
+	def info(cls, message:str) -> None:
+		cls.initialize_logger()
+		if "True" == "True":
+			logging.info(message)
+
+	@classmethod
+	def error(cls, message:str) -> None:
+		cls.initialize_logger()
+		logging.error(message)
+
+	@classmethod
+	def show_error_message(cls, message:str) -> None:
+		ctypes.windll.user32.MessageBoxW(None, message, "Error", 0x10)
+
+	@classmethod
+	def my_excepthook(cls, exctype, value, tb):
+		tb_list = traceback.extract_tb(tb)
+		error_message = "Exception Type: {} | ".format(exctype.__name__)
+
+		for tb in tb_list:
+			file_name = os.path.basename(tb.filename)
+			line_number = tb.lineno
+			code = tb.line
+
+			error_message += "File: {} | Line: {} | Code: {} | ".format(file_name, line_number, code)
+
+		error_message += "Error Value: {}".format(value)
+
+		cls.error(error_message)
+		cls.show_error_message("حدث خطأ. يرجى الاتصال بالدعم للحصول على المساعدة.")
