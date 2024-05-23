0799772c0e2f006cb6409b35efb7f7de2b89e991
mahmoud atef <109464635+MahmoudAtefFarook@users.noreply.github.com>
2024-05-23 22:25:43 +0300

    Update main menubar.

--- ui/widgets/menu_bar.py
+++ ui/widgets/menu_bar.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import QMenuBar
 from PyQt6.QtGui import QIcon, QAction
+from ui.dialogs.settings_dialog import SettingsDialog
 
 class MenuBar(QMenuBar):
     def __init__(self, parent=None):
@@ -8,23 +9,51 @@ class MenuBar(QMenuBar):
         self.create_menu()
 
     def create_menu(self):
-        file_menu = self.addMenu("&File")
-        new_action = QAction("&New", self)
-        file_menu.addAction(new_action)
-        open_action = QAction("&Open", self)
-        file_menu.addAction(open_action)
-        save_action = QAction("&Save", self)
-        file_menu.addAction(save_action)
-        file_menu.addSeparator()
-        exit_action = QAction("&Exit", self)
-        exit_action.setShortcut("Ctrl+Q")
-        exit_action.triggered.connect(self.close)
-        file_menu.addAction(exit_action)
+        navigation_menu = self.addMenu("التنقل")
+        next_action = QAction("التالي", self)
+        previous_action = QAction("السابق", self)
+        search_action = QAction("البحث", self)
+        quick_access_action = QAction("الوصول السريع", self)
 
-        edit_menu = self.addMenu("&Edit")
-        copy_action = QAction("&Copy", self)
-        edit_menu.addAction(copy_action)
-        cut_action = QAction("Cu&t", self)
-        edit_menu.addAction(cut_action)
-        paste_action = QAction("&Paste", self)
-        edit_menu.addAction(paste_action)
+
+        navigation_menu.addAction(next_action)
+        navigation_menu.addAction(previous_action)
+        navigation_menu.addAction(search_action)
+        navigation_menu.addAction(quick_access_action)
+
+        actions_menu = self.addMenu("الإجرائات")
+        save_position_action = QAction("حفظ الموضع الحالي", self)
+        surah_info_action = QAction("معلومات السورة", self)
+        verse_tafsir_action = QAction("تفسير الآية", self)
+        verse_info_action = QAction("معلومات الآية", self)
+        verse_grammar_action = QAction("اعراب الآية", self)
+        copy_verse_action = QAction("نسخ الآية", self)
+
+        actions_menu.addAction(save_position_action)
+        actions_menu.addAction(surah_info_action)
+        actions_menu.addAction(verse_tafsir_action)
+        actions_menu.addAction(verse_info_action)
+        actions_menu.addAction(verse_grammar_action)
+        actions_menu.addAction(copy_verse_action)
+
+        preferences_menu = self.addMenu("التفضيلات")
+        settings_action = QAction("الإعدادات", self)
+        settings_action.triggered.connect(self.OnSettings)
+        theme_change_action = QAction("تغيير الثيم", self)
+        text_direction_action = QAction("تغيير اتجاه النص", self)
+        
+        preferences_menu.addAction(settings_action)
+        preferences_menu.addAction(theme_change_action)
+        preferences_menu.addAction(text_direction_action)
+
+        help_menu = self.addMenu("المساعدة")
+        user_guide_action = QAction("دليل البرنامج", self)
+        contact_us_action = QAction("اتصل بنا", self)
+        update_program_action = QAction("تحديث البرنامج", self)
+
+        help_menu.addAction(user_guide_action)
+        help_menu.addAction(contact_us_action)
+        help_menu.addAction(update_program_action)
+
+    def OnSettings(self):
+        SettingsDialog().exec()
