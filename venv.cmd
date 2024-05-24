1b61f8813f5710263513cdfeb8ce112414715956
mahmoud atef <109464635+MahmoudAtefFarook@users.noreply.github.com>
2024-05-24 23:00:46 +0300

    Created venv.cmd file to create and activate the virtual environment with requirements.

---
+++ venv.cmd
@@ -0,0 +1,15 @@
+@echo off
+setlocal enabledelayedexpansion
+
+cd /d "%~dp0"
+
+set /p create_activate=Do you want to Create or Activate the environment? (C/A):
+
+if /i !create_activate! equ C (
+    start /wait python -m venv env
+          start cmd.exe /k "call env\Scripts\activate.bat && cd /d ""%~dp0"" && pip install -r requirements.txt"
+) else if /i !create_activate! equ A (
+    start cmd.exe /k "call env\Scripts\activate.bat"
+)
+
+exit
