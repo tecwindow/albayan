828803ecfe07c7e482ba4e3eaef05ed78800e95b
mahmoud atef <109464635+MahmoudAtefFarook@users.noreply.github.com>
2024-05-31 21:22:40 +0300

    Added UniversalSpeech.

---
+++ utils/universal_speech.py
@@ -0,0 +1,12 @@
+import  UniversalSpeech as u_speech
+from utils.settings import SettingsManager
+
+class UniversalSpeech:
+    universal_speech = u_speech.UniversalSpeech()
+    universal_speech.enable_native_speech(False)
+
+    @classmethod
+    def say(cls, msg: str) -> None:
+        if SettingsManager.current_settings["general"]["speak_actions_enabled"]:
+            cls.universal_speech.say(msg, True)
+
