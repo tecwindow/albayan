1253cc1488a8a0aeefa3aaa5f4e95cecd704f6d8
mahmoud atef <109464635+MahmoudAtefFarook@users.noreply.github.com>
2024-10-26 21:54:08 +0300

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import PyQt6
 pyqt_path = os.path.dirname(PyQt6.__file__)
 
 # Include additional files and DLLs
-include_files = [("database", "database"), ("Audio", "Audio"), ("bass.dll", "bass.dll")]
+include_files = [("database", "database"), ("Audio", "Audio"), ("bass.dll", "bass.dll"), ("icon.webp", "icon.webp")]
 dll_files = ["Qt6Core.dll", "Qt6Gui.dll", "Qt6Widgets.dll", "Qt6Network.dll"]
 for file in dll_files:
     include_files.append((os.path.join(pyqt_path, "Qt6", "bin", file), os.path.join("lib", file)))
@@ -17,7 +17,7 @@ build_exe_options = {
     "optimize": 1,
     "include_files": include_files,
     "packages": ["core_functions", "theme", "ui", "utils"],
-    "includes": ["PyQt6.QtCore", "PyQt6.QtWidgets", "PyQt6.QtGui", "PyQt6.QtMultimedia", "packaging", "requests", "UniversalSpeech"],
+    "includes": ["PyQt6.QtCore", "PyQt6.QtWidgets", "PyQt6.QtGui", "PyQt6.QtMultimedia", "packaging", "requests", "UniversalSpeech", "sqlalchemy", "sqlalchemy.dialects.sqlite", "apscheduler"],
     "excludes": ["tkinter", "test", "setuptools", "pip", "numpy"],
     "include_msvcr": True
 }
@@ -29,7 +29,7 @@ if sys.platform == "win32":
 
 setup(
     name="albayan",
-    version="1.2.4",
+    version="1.2.3",
     description=description,
     options={"build_exe": build_exe_options},
     executables=[Executable("main.py", base=base)]
