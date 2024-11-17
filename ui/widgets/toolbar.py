bc4a29c2257c506ca2523be53b6f732253fecd49
mahmoud atef <109464635+MahmoudAtefFarook@users.noreply.github.com>
2024-11-17 02:43:56 +0200

    Optmize AudioPlayerThread in toolbar.

--- ui/widgets/toolbar.py
+++ ui/widgets/toolbar.py
@@ -1,16 +1,16 @@
+import time
+from typing import Optional
 from PyQt6.QtWidgets import QToolBar, QPushButton, QSlider
-from PyQt6.QtCore import Qt, QTimer, QThread, pyqtSignal
+from PyQt6.QtCore import Qt, QThread, pyqtSignal
 from core_functions.Reciters import RecitersManager
 from utils.audio_player import AudioPlayer
 from utils.const import data_folder
 
 class AudioPlayerThread(QThread):
-    audio_status_changed = pyqtSignal(str)
-
-    def __init__(self, player, reciters, parent=None):
+    statusChanged = pyqtSignal()
+    def __init__(self, player: AudioPlayer, parent: Optional[object]=None):
         super().__init__(parent)
         self.player = player
-        self.reciters = reciters
         self.url = None
 
     def run(self):
@@ -18,21 +18,18 @@ class AudioPlayerThread(QThread):
             if self.player.source != self.url or self.player.is_stopped():
                 self.player.load_audio(self.url)
             self.player.play()
-            self.audio_status_changed.emit("إيقاف مؤقت")
+            self.statusChanged.emit()
+
+        while self.player.is_playing():
+            time.sleep(0.1)
+        else:
+            self.statusChanged.emit()
         
-    def pause_audio(self):
-        self.player.pause()
-        self.audio_status_changed.emit("استماع الآية الحالية")
-
-    def stop_audio(self):
-        self.player.stop()
-        self.audio_status_changed.emit("استماع الآية الحالية")
-
-    def set_audio_url(self, url):
+    def set_audio_url(self, url: str):
         self.url = url
 
 class AudioToolBar(QToolBar):
-    def __init__(self, parent=None):
+    def __init__(self, parent: Optional[object]=None):
         super().__init__(parent)
         self.parent = parent
         self.player = AudioPlayer()
@@ -40,15 +37,10 @@ class AudioToolBar(QToolBar):
         
         self.play_pause_button = self.create_button("استماع الآية الحالية", self.toggle_play_pause)
         self.stop_button = self.create_button("إقاف", self.stop_audio)
-
         self.volume_slider = self.create_slider(0, 100, 50, self.change_volume)
 
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.check_audio_status)
-        self.timer.start(500)
-
-        self.audio_thread = AudioPlayerThread(self.player, self.reciters)
-        self.audio_thread.audio_status_changed.connect(self.update_play_pause_button_text)
+        self.audio_thread = AudioPlayerThread(self.player, self.parent)
+        self.audio_thread.statusChanged.connect(self.update_play_pause_button_text)
 
     def create_button(self, text, callback):
         button = QPushButton(text)
@@ -68,23 +60,18 @@ class AudioToolBar(QToolBar):
 
     def toggle_play_pause(self):
         if self.player.is_playing():
-            self.audio_thread.pause_audio()
+            self.player.pause()
         else:                
             ayah_info = self.parent.get_current_ayah_info()
             url = self.reciters.get_url(68, ayah_info[0], ayah_info[3])
             self.audio_thread.set_audio_url(url)
             self.audio_thread.start()
 
-        self.update_play_pause_button_text()
-
     def stop_audio(self):
-        self.audio_thread.stop_audio()
+        self.player.stop()
 
     def change_volume(self, value):
         self.player.set_volume(value / 100)
-
-    def check_audio_status(self):
-        self.update_play_pause_button_text()
 
     def update_play_pause_button_text(self):
         label = "إيقاف مؤقت" if self.player.is_playing() else "استماع الآية الحالية"
