b997ec3849223769b86e5f5123560f4e0fc56e82
Qais <ww258148@gmail.com>
2024-12-05 18:28:09 +0300

    Activated  numpad enter with the buttons in the main window.

--- ui/widgets/button.py
+++ ui/widgets/button.py
@@ -6,6 +6,6 @@ from core_functions.tafaseer import Category
 class EnterButton(QPushButton):
     def keyPressEvent(self, event):
 
-        if event.key() == Qt.Key.Key_Return or event.key() == Qt.Key.Key_Space:
+        if event.key() in {Qt.Key.Key_Return, Qt.Key.Key_Space, Qt.Key.Key_Enter}:
             self.clicked.emit()
 
