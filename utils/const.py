af4581d47be7432fae52d0043a53402cd92e2e78
Qais <ww258148@gmail.com>
2025-03-10 18:19:34 +0300

    updated version.

--- utils/const.py
+++ utils/const.py
@@ -24,7 +24,7 @@ os.makedirs(albayan_documents_dir, exist_ok=True)
 # program information
 program_name = "البيان"
 program_english_name = "Albayan"
-program_version = "3.0.0"
+program_version = "3.0.1"
 program_icon = "Albayan.ico"
 website = "https://tecwindow.net/"
 
