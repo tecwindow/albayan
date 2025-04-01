be65decb86a8b12166e3777b3b414b0d65100a85
Qais <ww258148@gmail.com>
2025-04-01 12:38:03 +0300

    Added logs to ayah_player.py.

--- utils/audio_player/ayah_player.py
+++ utils/audio_player/ayah_player.py
@@ -1,10 +1,13 @@
 from PyQt6.QtWidgets import QMessageBox
 from .bass_player import AudioPlayer
 from utils.settings import Config
-
-
+from utils.logger import LoggerManager
+logger = LoggerManager.get_logger(__name__)
 class AyahPlayer(AudioPlayer):
     instances = []
     def __init__(self) -> None:
         super().__init__(Config.audio.ayah_volume_level, device=Config.audio.ayah_device)
         AyahPlayer.instances.append(self)
+        logger.info("AyahPlayer initialized with volume level: %d and device: %s",
+                    Config.audio.ayah_volume_level, Config.audio.ayah_device)
+
