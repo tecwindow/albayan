94cad2209e0b622a8c1af0dd9b060952432c4f8c
Qais <ww258148@gmail.com>
2025-04-01 12:39:09 +0300

    Added logs to sura_player.py.

--- utils/audio_player/sura_player.py
+++ utils/audio_player/sura_player.py
@@ -1,10 +1,15 @@
 from PyQt6.QtWidgets import QMessageBox
 from .bass_player import AudioPlayer
 from utils.settings import Config
-
+from utils.logger import LoggerManager
+logger = LoggerManager.get_logger(__name__)
 
 class SurahPlayer(AudioPlayer):
     instances = []
     def __init__(self) -> None:
         super().__init__(Config.audio.surah_volume_level, device=Config.audio.surah_device)
         SurahPlayer.instances.append(self)
+        logger.info("SurahPlayer initialized with volume level: %d and device: %s",
+                    Config.audio.surah_volume_level, Config.audio.surah_device)
+        
+        
